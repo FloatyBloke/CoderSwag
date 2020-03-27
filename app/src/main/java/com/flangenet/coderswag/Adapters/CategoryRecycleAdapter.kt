e123770b80eaf645dd0cff9464461e2c9f5b57c9
Wak <LeeTenerife@GMail.Com>
2020-03-27 15:56:18 +0000

    lesson 6

--- app/src/main/java/com/flangenet/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/flangenet/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -11,7 +11,7 @@ import com.flangenet.coderswag.Model.Category
 import com.flangenet.coderswag.R
 import kotlinx.android.synthetic.main.category_list_item.view.*
 
-class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>, val itemClick: (Category) -> Unit) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
 
 
 
@@ -26,10 +26,10 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context)
             .inflate(R.layout.category_list_item,parent,false)
-        return Holder(view)
+        return Holder(view, itemClick)
     }
 
-    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
+    inner class Holder(itemView: View?, val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView!!){
 
         val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
         val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
@@ -38,6 +38,7 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
             val resourceId =  context.resources.getIdentifier(category.image,"drawable",context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text = category.title
+            itemView.setOnClickListener{ itemClick(category)}
         }
     }
 }
